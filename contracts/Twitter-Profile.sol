7fbfd8ad39e62026e02a52ac65a0dc54f491767a
usmanahmednetixsol <usman-ahmed@netixsol.com>
2024-08-04 22:29:08 +0500

    Add the Profile functionality and ownable openZiplender contract

---
+++ contracts/Twitter-Profile.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+// 1️⃣ Save UserProfile to the mapping in the setProfile() function
+// HINT: don't forget to set the _displayName and _bio
+
+pragma solidity ^0.8.0;
+
+contract Profile {
+    struct UserProfile {
+        string displayName;
+        string bio;
+    }
+
+    mapping(address => UserProfile) public profiles;
+
+    function setProfile(string memory _displayName, string memory _bio)
+        external
+    {
+        // CODE HERE 👇
+        profiles[msg.sender] = UserProfile({
+            displayName: _displayName,
+            bio: _bio
+        });
+    }
+
+    function getProfile(address _user)
+        public
+        view
+        returns (UserProfile memory)
+    {
+        return profiles[_user];
+    }
+}
--- contracts/TwitterContract.sol
+++ contracts/TwitterContract.sol
@@ -1,7 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TwitterContract {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IProfile {
+    struct UserProfile {
+        string displayName;
+        string bio;
+    }
+
+  function setProfile(string memory _displayName, string memory _bio) external ;
+function getProfile(address _user) external view returns (UserProfile memory);
+}
+
+contract TwitterContract is Ownable {
     struct Tweet {
         uint256 id;
         address author;
@@ -17,18 +29,35 @@ contract TwitterContract {
 
     mapping(address => TweetWithLikes[]) public tweets;
     uint16 public MAX_TWEET_LENGTH = 280;
-    address public owner;
 
-    constructor() {
-        owner = msg.sender;
+    IProfile ProfileAddress;
+
+    event createTweetEvent(
+        uint256 id,
+        address author,
+        string content,
+        uint256 datePublished
+    );
+    event LikeTweet(
+        address liker,
+        uint256 tweetId,
+        address tweetAuthor,
+        uint256 likesCount
+    );
+    event UnlikeTweet(
+        address unliker,
+        uint256 tweetId,
+        address tweetAuthor,
+        uint256 likesCount
+    );
+
+    constructor(address initialOwner, address _profileContractAddress) Ownable(initialOwner) {
+        ProfileAddress = IProfile( _profileContractAddress);
     }
 
-    event createTweetEvent(uint256 id,address author, string content,uint256 datePublished);
-    event LikeTweet(address liker,uint256 tweetId,address tweetAuthor, uint256 likesCount);
-    event UnlikeTweet(address unliker,uint256 tweetId,address tweetAuthor, uint256 likesCount);
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "You are not the owner");
+    modifier RegisterOnly(){
+        IProfile.UserProfile memory user = ProfileAddress.getProfile(msg.sender);
+        require(bytes(user.displayName).length > 0, "You are not register yet");
         _;
     }
 
@@ -36,7 +65,7 @@ contract TwitterContract {
         MAX_TWEET_LENGTH = _length;
     }
 
-    function createTweet(string memory _tweet) public {
+    function createTweet(string memory _tweet) public RegisterOnly {
         require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Tweet too long");
 
         TweetWithLikes storage newTweet = tweets[msg.sender].push();
@@ -45,10 +74,15 @@ contract TwitterContract {
         newTweet.tweet.content = _tweet;
         newTweet.tweet.datePublished = block.timestamp;
         newTweet.tweet.likesCount = 0;
-        emit createTweetEvent(tweets[msg.sender].length - 1, msg.sender, _tweet, block.timestamp);
+        emit createTweetEvent(
+            tweets[msg.sender].length - 1,
+            msg.sender,
+            _tweet,
+            block.timestamp
+        );
     }
 
-    function like(address _author, uint256 _id) external {
+    function like(address _author, uint256 _id) external RegisterOnly {
         require(tweets[_author].length > _id, "Tweet doesn't exist");
 
         TweetWithLikes storage twt = tweets[_author][_id];
@@ -59,7 +93,7 @@ contract TwitterContract {
         emit LikeTweet(msg.sender, _id, _author, twt.tweet.likesCount);
     }
 
-    function unlike(address _author, uint256 _id) external {
+    function unlike(address _author, uint256 _id) external RegisterOnly {
         require(tweets[_author].length > _id, "Tweet doesn't exist");
 
         TweetWithLikes storage twt = tweets[_author][_id];
@@ -70,18 +104,40 @@ contract TwitterContract {
         emit UnlikeTweet(msg.sender, _id, _author, twt.tweet.likesCount);
     }
 
-    function totalLikes(address _author, uint256 _id) public view returns (uint256) {
+    function totalLike(address _author) public view returns (uint256) {
+        uint256 totalTweetsLikes;
+
+        for (uint256 i = 0; i < tweets[_author].length; i++) {
+            totalTweetsLikes += tweets[_author][i].tweet.likesCount;
+        }
+
+        return totalTweetsLikes;
+    }
+
+    function tweetTotalLikes(address _author, uint256 _id)
+        public
+        view
+        returns (uint256)
+    {
         require(tweets[_author].length > _id, "Tweet doesn't exist");
 
         return tweets[_author][_id].tweet.likesCount;
     }
 
-    function getTweet(address _author, uint _i) public view returns (Tweet memory) {
+    function getTweet(address _author, uint256 _i)
+        public
+        view
+        returns (Tweet memory)
+    {
         require(tweets[_author].length > _i, "Tweet doesn't exist");
         return tweets[_author][_i].tweet;
     }
 
-    function getAllTweets(address _author) public view returns (Tweet[] memory) {
+    function getAllTweets(address _author)
+        public
+        view
+        returns (Tweet[] memory)
+    {
         uint256 length = tweets[_author].length;
         Tweet[] memory allTweets = new Tweet[](length);
         for (uint256 i = 0; i < length; i++) {
